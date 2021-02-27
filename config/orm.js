ea1a07fe28529aa536713a3f8ccd2de2b491b4f5
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-02-27 08:42:30 -0500

    Added coding to server.js. Established engine requirements for handlebars. Set require routes for controller

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,29 @@
+const { insertOne } = require('../models/burger.js');
 const connection = require('./connection.js');
 
-selectAll();
+const orm = {
+    selectAll(tableInput, cb){
+        const queryString = `Select * FROM ${tableInput};`;
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne(table, cols, vals, cb){
+        const queryString = `INSERT INTO ${table}`;
+
+        
+
+    }
+
+
+
+
+}
+
+//selectAll();
 insertOne();
 updateOne();
 
--- server.js
+++ server.js
@@ -3,3 +3,21 @@ const express = require('express');
 const port = process.env.PORT || 8080
 
 const app = express ();
+
+// create app use for express static folder
+
+app.use(express.urlencoded ({ extended: true}));
+app.use(express.json());
+
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs ({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+const routes = require('./controllers/burgers_controller.js');
+
+app.use(routes);
+
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
