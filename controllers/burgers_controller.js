547f31fd13ab738436cd3c896a09f153d5c60604
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-02-27 08:08:33 -0500

    Created config file with orms.js. Created Controllers files with burgers_controller.js. Created models burger.js. Added code and module exports to all files.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+const router = express.Router();
+
+const burger = require('../models/burger.js');
+
+router.get('/', (req,res) => {
+    burger.all((data) => {
+        const hbsObject = {
+            burgers: data,
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
+
+router.post('/api/burgers', (req, res) => {
+    burgers.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
+        res.json({ id: result.insertId });
+    })
+});
+
+router.put('/api/burgers/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+
+    console.log('condition', condition);
+
+    burgers.update(
+        {
+            devoured: req.body.devoured,
+        },
+        condition,
+        (result) => {
+            if (result.changedRows === 0){
+                return res.status(404).end(0);
+            }
+            res.status(200).end();
+        }
+    );
+});
+
+module.exports = router;
