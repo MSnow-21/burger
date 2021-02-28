18fdde765f7fbc3c2499d737370b87ed81eef60c
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-02-28 11:01:53 -0500

    Created insertOne function into burger.js and orm.js. Added helper queryString function.

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,16 @@
 const connection = require('./connection.js');
 
+//Creating helper functions for insert methods like class activities
+
+const questionMarks = (num) => {
+    const arr = [];
+
+    for (let i =0; i < num; i++){
+        arr.push('?');
+    }
+    return arr.toString();
+}
+
 const orm = {
     selectAll(tableInput, cb){
         const queryString = `Select * FROM ${tableInput};`;
@@ -9,6 +20,27 @@ const orm = {
             }
             cb(result);
         });
+    },
+    insertOne(table,cols,vals,cb){
+        let queryString = 'INSERT INTO ${table}';
+
+        queryString += ' (';
+        queryString += cols.toString();
+        queryString += ') ';
+        queryString += 'VALUES (';
+        queryString += questionMarks(val.length);
+        queryString += ') ';
+
+        console.log(queryString);
+
+        connection.query(queryString);
+
+        connection.query(queryString, vals, (err, result) => {
+            if (err) {;
+            throw err;
+        }
+        cb(result);
+    });
     },
     // insertOne(vals, cb){
     //     //
@@ -27,4 +59,4 @@ const orm = {
 // insertOne();
 // updateOne();
 
-//module.exports = orm
+module.exports = orm
--- models/burger.js
+++ models/burger.js
@@ -4,7 +4,9 @@ const burger = {
     selectAll(cb){
         orm.selectAll('burgers', (res) => cb(res));
     },
-
+    insertOne(cols,vals,cb){
+        orm.insertOne('burgers', cols, vals, (res) => cb(res));
+    },
     // insertOne(cols, vals, cb){
     //     orm.insertOne('burgers', cols, vals, (res) => cb(res));
     // },
