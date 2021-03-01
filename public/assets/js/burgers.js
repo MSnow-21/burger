c89a01ab361ccf9ea80507c73d43840eaa7e71dd
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-02-28 21:28:53 -0500

    Created additional code for devoured burger button. General updates.

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -3,8 +3,43 @@ document.addEventListener('DOMContentLoaded', (event)=> {
         console.log('DOM loaded');
     }
 
-    //create a burger button//
-    //Creating initial code//
+    //Devouring the burger initial code
+
+    const devourBurgerButton = document.getElementById('.devoured-burger');
+
+    if(devourBurgerButton){
+        devourBurgerButton.forEach((button) => {
+            button.addEventListener('click', (event) => {
+                console.log('test');
+
+                const id = event.target.getAttribute('data-devourburger');
+
+                const newBurgerState = {
+                    devoured: newDevoured,
+                };
+
+                fetch(`api/burgers/${id}`, {
+                    method: 'PUT',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+
+                    body: JSON.stringify(newBurgerState);
+                }).then((response) => {
+                    if(response.ok){
+                        console.log(`changed sleep to: ${newDevoured}`);
+                        location.reload('/');
+                    }else{
+                        alert('something went wrong!')
+                    }
+                });
+            });
+
+        });
+    }
+
+    //Creating the burger initial code
 
     const createBurgerButton = document.getElementById('burger-form');
 
@@ -26,7 +61,7 @@ document.addEventListener('DOMContentLoaded', (event)=> {
         }).then(()=> {
             document.getElementById('bur').value = '';
 
-            console.log('Created a new cat!');
+            console.log('Created a new burger!');
             location.reload();
         });
     };
